752621993947988167ab60085de56b432a40f9a0
alexandre <alexandresch@hotmail.com>
2022-10-19 14:15:12 +0200

    Phase 2 : premier test "Hello world !

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,20 +4,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 /**
  * Description of AccueilController
  *
  * @author sandd
  */
-class AccueilController {
+class AccueilController extends AbstractController{
     /**
      * @Route("/", name="acceuil")
      * @return Response
      */
     public function index(): Response{
-        return new Response ('hello world');
+        return $this->render("pages/acceuil.html.twig");
     }
 }
