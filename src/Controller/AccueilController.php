ddcfbbf5af061a009c147c00a8b453b4541fc5ef
alexandre <alexandresch@hotmail.com>
2022-10-19 13:51:56 +0200

    Phase 2 : premier test "Hello world !

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Description of AccueilController
+ *
+ * @author sandd
+ */
+class AccueilController {
+    /**
+     * @Route("/", name="acceuil")
+     * @return Response
+     */
+    public function index(): Response{
+        return new Response ('hello world');
+    }
+}
